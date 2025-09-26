651f71e479d7d1450571f83b309078e8a11176de
Raj Mishra <168812828+MishrajiiCode@users.noreply.github.com>
2025-09-26 16:58:03 +0530

    Update app.js

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const CHANGELOG = {
     ]
 };
 // ===================== APP CONFIGURATION =====================
-const APP_VERSION = '1.5.14-Beta.'; // Increment this to show an update notification
+const APP_VERSION = '1.5.12-Beta.'; // Increment this to show an update notification
 
 // ===================== GLOBAL STATE VARIABLES =====================
 let currentSubject = '';
@@ -1607,3 +1607,4 @@ function removeBookmark(index) {
         displayBookmarkedQuestions();
     }
 }
+
